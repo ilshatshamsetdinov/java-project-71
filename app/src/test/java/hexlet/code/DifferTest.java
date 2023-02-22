9c0b04dd91d3ea25b854bc445a104914a146ca1a
Ilshat Shamsetdinov <myheroisceleste@gmail.com>
2023-02-22 21:25:55 +0300

    step 6

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.Test;
 public class DifferTest {
     private static String expected = """
             {
-              - follow: true
-                host: hex.io
-              - proxy: 1.1.1.1
-              - timeout: 60
-              + timeout: 40
-              + verbose: false
+            - follow: true
+              host: hex.io
+            - proxy: 1.1.1.1
+            - timeout: 60
+            + timeout: 40
+            + verbose: false
             }""";
     @Test
-    public static void testGenerate() throws Exception {
-        String readFilePath1 = "/test/resources/testFile2.json";
-        String readFilePath2 = "/test/resources/testFile2.json";
+    public void generateTest() throws Exception {
+        String readFilePath1 = "src/test/resources/testFile1.json";
+        String readFilePath2 = "src/test/resources/testFile2.json";
         assertEquals(expected, generate(readFilePath1, readFilePath2));
     }
 }
